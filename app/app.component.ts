37a852f32b09e6989e061cddd1d7a5d35ad95e8c
Catherine Beaucourt <catherine@beaucourt.be>
2016-03-29 22:18:32 +0200

    Basic setup

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from 'angular2/core';
+
+@Component({ //invoke with metadata object
+    selector: 'my-app', // css selector
+    template: '<h1>Begroting Vlaanderen!</h1>'
+})
+export class AppComponent { }
---
+++ app/main.ts
@@ -0,0 +1,4 @@
+import {bootstrap}    from 'angular2/platform/browser'; // choose a different bootstrapper for better SEO
+import {AppComponent} from './app.component';
+
+bootstrap(AppComponent);
