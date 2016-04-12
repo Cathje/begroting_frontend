e10c9f8319d3b768c7c35e769acacc5b87fd1b1a
Catherine Beaucourt <catherine@beaucourt.be>
2016-04-12 20:58:19 +0200

    fixed run from webstorm command

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,18 @@
-import {bootstrap}    from 'angular2/platform/browser'; // choose a different bootstrapper for better SEO
-import {AppComponent} from './app.component';
+import {Component, provide} from 'angular2/core';
+import {bootstrap}    from 'angular2/platform/browser'
+import {AppComponent} from './app.component'
+import {HTTP_PROVIDERS} from "angular2/http";
 
-bootstrap(AppComponent);
+import {
+    APP_BASE_HREF,
+    ROUTER_DIRECTIVES,
+    ROUTER_PROVIDERS,
+    RouteConfig,
+    ROUTER_PRIMARY_COMPONENT,
+} from "angular2/router";
+
+
+bootstrap(AppComponent,
+    [    ROUTER_PROVIDERS,
+        provide(APP_BASE_HREF,            {useValue: "/"}),
+    ]);
