8e3a882de3d4312df919f6185fee3d5526b1d37e
Kevin Laerte <Kevin Laerte>
2016-05-14 11:20:49 +0200

    basic styling login

--- app/components/mainComponents/budget/expenses.component.js
+++ app/components/mainComponents/budget/expenses.component.js
@@ -1,6 +1,4 @@
-System.register(['angular2/core', './../../../services/townService.component.js', 'angular2/http', 'angular2/router', './../../subComponents/input/townSelector.component.js', './../../subComponents/input/editableField.component.js', "../../../models/mainTown.js", './../../subComponents/graphs/sunburst.component.js', "../../../services/begrotingService.js", "../../../services/ActieService.js"], function(exports_1, context_1) {
-    "use strict";
-    var __moduleName = context_1 && context_1.id;
+System.register(['angular2/core', './../../../services/townService.component.js', 'angular2/http', 'angular2/router', './../../subComponents/input/townSelector.component.js', './../../subComponents/input/editableField.component.js', "../../../models/mainTown.js", './../../subComponents/graphs/sunburst.component.js', "../../../services/begrotingService.js", "../../../services/ActieService.js"], function(exports_1) {
     var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
         var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
         if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -109,7 +107,7 @@ System.register(['angular2/core', './../../../services/townService.component.js'
                 ], ExpensesComponent);
                 return ExpensesComponent;
                 var _a, _b, _c;
-            }());
+            })();
             exports_1("ExpensesComponent", ExpensesComponent);
         }
     }
