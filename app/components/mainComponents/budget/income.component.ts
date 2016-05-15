e76af33a0c0e850f9be5f6562d43e4acaebf92b0
Catherine Beaucourt <catherine@beaucourt.be>
2016-05-15 02:09:40 +0200

    bugfix

--- app/components/mainComponents/budget/income.component.ts
+++ app/components/mainComponents/budget/income.component.ts
@@ -20,7 +20,7 @@ import {GemeenteCategorie} from "../../../models/gemeenteCategorie";
     template: `
         <div class="container">
         <section class="intro col-xs-12">
-            <h1>De uitgaves van {{mainTown?.naam}}</h1>
+            <h1>De inkomsten van {{mainTown?.naam}}</h1>
             <p>Hier komt een paragraaf.Similiquecilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et</p>
         <div class="clearfix">
         <div class="graph col-xs-12 col-sm-8" (window:resize)="onResize($event)">
