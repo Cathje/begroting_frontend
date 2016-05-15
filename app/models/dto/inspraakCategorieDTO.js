60625594b93ae9283ad2964390ebe241f3857a92
Catherine Beaucourt <catherine@beaucourt.be>
2016-05-15 13:07:59 +0200

    no message

--- app/models/dto/inspraakCategorieDTO.js
+++ app/models/dto/inspraakCategorieDTO.js
@@ -1,4 +1,6 @@
-System.register([], function(exports_1) {
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var InspraakCategorie;
     return {
         setters:[],
@@ -11,7 +13,7 @@ System.register([], function(exports_1) {
                     this.acties = null;
                 }
                 return InspraakCategorie;
-            })();
+            }());
             exports_1("InspraakCategorie", InspraakCategorie);
         }
     }
--- app/models/project.js
+++ app/models/project.js
@@ -1,4 +1,6 @@
-System.register([], function(exports_1) {
+System.register([], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
     var Project;
     return {
         setters:[],
@@ -16,7 +18,7 @@ System.register([], function(exports_1) {
                     this.gemeente = "Gent";
                 }
                 return Project;
-            })();
+            }());
             exports_1("Project", Project);
         }
     }
