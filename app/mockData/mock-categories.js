3bb28cd74721b6248e5208ab75259874adcb15cb
Catherine Beaucourt <catherine@beaucourt.be>
2016-05-20 01:02:56 +0200

    no message

--- app/mockData/mock-categories.js
+++ app/mockData/mock-categories.js
@@ -6,16 +6,16 @@ System.register([], function(exports_1, context_1) {
         setters:[],
         execute: function() {
             exports_1("CATEGORIES", CATEGORIES = [
-                { id: "01", naam: "Algemeen bestuur", "kleur": "#fff66c", "icoon": "glyphicon-globe", "afbeelding": null },
+                { id: "00", naam: "Algemene financiering", "kleur": "#999999", "icoon": "glyphicon-euro", "afbeelding": null },
+                { id: "01", naam: "Algemeen bestuur", "kleur": "#cccccc", "icoon": "glyphicon-globe", "afbeelding": null },
                 { id: "02", naam: "Zich verplaatsen en mobiliteit", "kleur": "#f7bdc7", "icoon": "glyphicon-plane", "afbeelding": null },
                 { id: "03", naam: "Natuur en milieubeheer", "kleur": "#d0d257", "icoon": "glyphicon-tree-deciduous", "afbeelding": null },
                 { id: "04", naam: "Veiligheidszorg", "kleur": "#ff8e6c", "icoon": "glyphicon-warning-sign", "afbeelding": null },
                 { id: "05", naam: "Ondernemen en werken", "kleur": "#00cad2", "icoon": "glyphicon-lock", "afbeelding": null },
                 { id: "06", naam: "Wonen en ruimtelijke ordening", "kleur": "#80d9be", "icoon": "glyphicon-home", "afbeelding": null },
                 { id: "07", naam: "Cultuur en vrije tijd", "kleur": "#ce205e", "icoon": "glyphicon-glass", "afbeelding": null },
-                { id: "08", naam: "Leren en onderwijs", "kleur": "#efb3e9", "icoon": "glyphicon-education", "afbeelding": null },
+                { id: "08", naam: "Leren en onderwijs", "kleur": "#fff66c", "icoon": "glyphicon-education", "afbeelding": null },
                 { id: "09", naam: "Zorg en opvang", "kleur": "#41526c", "icoon": "glyphicon-heart", "afbeelding": null },
-                { id: "00", naam: "Algemene financiering", "kleur": "#999999", "icoon": "glyphicon-euro", "afbeelding": null },
             ]);
         }
     }
